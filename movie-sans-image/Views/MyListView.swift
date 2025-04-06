55d6250cf0cb68ba6d58e0d40925a9488cb30b24
Adam Pagels <pagels@hotmail.ca>
2025-04-06 11:55:47 +0100

    Create MyListView

---
+++ movie-sans-image/Views/MyListView.swift
@@ -0,0 +1,18 @@
+//
+//  MyListView.swift
+//  movie-sans-image
+//
+//  Created by Adam Pagels on 2025-04-06.
+//
+
+import SwiftUI
+
+struct MyListView: View {
+    var body: some View {
+        Text("My List View")
+    }
+}
+
+#Preview {
+    MyListView()
+}
