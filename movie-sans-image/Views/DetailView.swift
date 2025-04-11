c35e9c5ad9aa0878511cf982ca3f08f43501934e
Adam Pagels <pagels@hotmail.ca>
2025-04-11 17:55:31 +0100

    Create View

---
+++ movie-sans-image/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  movie-sans-image
+//
+//  Created by Adam Pagels on 2025-04-11.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("DetailView")
+    }
+}
+
+#Preview() {
+    DetailView()
+}
