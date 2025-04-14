37c42c8306d986d00c8c51e9e15b60c2f36906a0
Adam Pagels <pagels@hotmail.ca>
2025-04-14 21:20:10 +0100

    Create Neubrutal container

---
+++ movie-sans-image/Views/Components/NeubrutalContainerView.swift
@@ -0,0 +1,26 @@
+//
+//  NeubrutalContainerView.swift
+//  movie-sans-image
+//
+//  Created by Adam Pagels on 2025-04-14.
+//
+
+import SwiftUI
+
+struct NeubrutalContainerView<Content: View>: View {
+    @ViewBuilder var content: Content
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .offset(x: 3, y: 5)
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundColor(.gray)
+
+            content
+
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+        }
+    }
+}
