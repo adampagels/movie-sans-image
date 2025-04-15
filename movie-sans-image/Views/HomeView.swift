c09fbac5da7e002232a20b5ba7dcb17c72b4080d
Adam Pagels <pagels@hotmail.ca>
2025-04-15 21:15:40 +0100

    Add scrolling movie categories

--- movie-sans-image/Views/HomeView.swift
+++ movie-sans-image/Views/HomeView.swift
@@ -7,12 +7,35 @@
 
 import SwiftUI
 
+enum MovieListCategory: String, CaseIterable {
+    case popular = "Popular"
+    case nowPlaying = "Now Playing"
+    case topRated = "Top Rated"
+    case upcoming = "Upcoming"
+}
+
 struct HomeView: View {
     @State private var movieViewModel: MovieViewModel = .init(apiService: APIService())
     @State var watchlistViewModel: WatchlistViewModel
 
     var body: some View {
         List {
+            ScrollView(.horizontal) {
+                HStack(spacing: 16) {
+                    ForEach(MovieListCategory.allCases, id: \.rawValue) { category in
+                        NeubrutalContainerView {
+                            Text(category.rawValue)
+                                .padding()
+                        }
+                        .fixedSize()
+                    }
+                }
+                .padding([.trailing, .leading], 3)
+                .padding(.vertical)
+                .listRowSeparator(.hidden)
+            }
+            .scrollIndicators(.hidden)
+
             ForEach(movieViewModel.latestMovies) { movie in
                 NeubrutalContainerView {
                     HStack {
