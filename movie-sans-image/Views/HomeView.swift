328b1e22c8a2546562dae8000d7f5ac19f2942e1
Adam Pagels <pagels@hotmail.ca>
2025-05-20 21:45:26 +0100

    Add tmdb credits

--- movie-sans-image/Views/HomeView.swift
+++ movie-sans-image/Views/HomeView.swift
@@ -13,6 +13,18 @@ struct HomeView: View {
 
     var body: some View {
         VStack {
+            VStack(alignment: .leading) {
+                Image("TMDBLogo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100)
+
+                Text("This product uses the TMDB API but is not endorsed or certified by TMDB.")
+                    .font(.custom("Futura", size: 10))
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading)
+
             CategoryList(
                 selectedCategory: homeViewModel.selectedCategory,
                 onSelect: { (category: MovieListCategory) in
